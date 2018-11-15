851b0b98eab16b1fb4e3ea6f1dc17a05c6398179
Cassie Wilson <contact@transtracks.app>
2018-11-15 17:14:52 +1100

    Remove deprecated classes from KotterKnife
    Removing DialogFragment and Fragment from the Kotterknife implementation.

--- app/src/main/java/kotterknife/ButterKnife.kt
+++ app/src/main/java/kotterknife/ButterKnife.kt
@@ -2,8 +2,6 @@ package kotterknife
 
 import android.app.Activity
 import android.app.Dialog
-import android.app.DialogFragment
-import android.app.Fragment
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
@@ -18,12 +16,8 @@ public fun <V : View> Activity.bindView(id: Int)
         : ReadOnlyProperty<Activity, V> = required(id, viewFinder)
 public fun <V : View> Dialog.bindView(id: Int)
         : ReadOnlyProperty<Dialog, V> = required(id, viewFinder)
-public fun <V : View> DialogFragment.bindView(id: Int)
-        : ReadOnlyProperty<DialogFragment, V> = required(id, viewFinder)
 public fun <V : View> SupportDialogFragment.bindView(id: Int)
         : ReadOnlyProperty<SupportDialogFragment, V> = required(id, viewFinder)
-public fun <V : View> Fragment.bindView(id: Int)
-        : ReadOnlyProperty<Fragment, V> = required(id, viewFinder)
 public fun <V : View> SupportFragment.bindView(id: Int)
         : ReadOnlyProperty<SupportFragment, V> = required(id, viewFinder)
 public fun <V : View> ViewHolder.bindView(id: Int)
@@ -35,12 +29,8 @@ public fun <V : View> Activity.bindOptionalView(id: Int)
         : ReadOnlyProperty<Activity, V?> = optional(id, viewFinder)
 public fun <V : View> Dialog.bindOptionalView(id: Int)
         : ReadOnlyProperty<Dialog, V?> = optional(id, viewFinder)
-public fun <V : View> DialogFragment.bindOptionalView(id: Int)
-        : ReadOnlyProperty<DialogFragment, V?> = optional(id, viewFinder)
 public fun <V : View> SupportDialogFragment.bindOptionalView(id: Int)
         : ReadOnlyProperty<SupportDialogFragment, V?> = optional(id, viewFinder)
-public fun <V : View> Fragment.bindOptionalView(id: Int)
-        : ReadOnlyProperty<Fragment, V?> = optional(id, viewFinder)
 public fun <V : View> SupportFragment.bindOptionalView(id: Int)
         : ReadOnlyProperty<SupportFragment, V?> = optional(id, viewFinder)
 public fun <V : View> ViewHolder.bindOptionalView(id: Int)
@@ -52,12 +42,8 @@ public fun <V : View> Activity.bindViews(vararg ids: Int)
         : ReadOnlyProperty<Activity, List<V>> = required(ids, viewFinder)
 public fun <V : View> Dialog.bindViews(vararg ids: Int)
         : ReadOnlyProperty<Dialog, List<V>> = required(ids, viewFinder)
-public fun <V : View> DialogFragment.bindViews(vararg ids: Int)
-        : ReadOnlyProperty<DialogFragment, List<V>> = required(ids, viewFinder)
 public fun <V : View> SupportDialogFragment.bindViews(vararg ids: Int)
         : ReadOnlyProperty<SupportDialogFragment, List<V>> = required(ids, viewFinder)
-public fun <V : View> Fragment.bindViews(vararg ids: Int)
-        : ReadOnlyProperty<Fragment, List<V>> = required(ids, viewFinder)
 public fun <V : View> SupportFragment.bindViews(vararg ids: Int)
         : ReadOnlyProperty<SupportFragment, List<V>> = required(ids, viewFinder)
 public fun <V : View> ViewHolder.bindViews(vararg ids: Int)
@@ -69,12 +55,8 @@ public fun <V : View> Activity.bindOptionalViews(vararg ids: Int)
         : ReadOnlyProperty<Activity, List<V>> = optional(ids, viewFinder)
 public fun <V : View> Dialog.bindOptionalViews(vararg ids: Int)
         : ReadOnlyProperty<Dialog, List<V>> = optional(ids, viewFinder)
-public fun <V : View> DialogFragment.bindOptionalViews(vararg ids: Int)
-        : ReadOnlyProperty<DialogFragment, List<V>> = optional(ids, viewFinder)
 public fun <V : View> SupportDialogFragment.bindOptionalViews(vararg ids: Int)
         : ReadOnlyProperty<SupportDialogFragment, List<V>> = optional(ids, viewFinder)
-public fun <V : View> Fragment.bindOptionalViews(vararg ids: Int)
-        : ReadOnlyProperty<Fragment, List<V>> = optional(ids, viewFinder)
 public fun <V : View> SupportFragment.bindOptionalViews(vararg ids: Int)
         : ReadOnlyProperty<SupportFragment, List<V>> = optional(ids, viewFinder)
 public fun <V : View> ViewHolder.bindOptionalViews(vararg ids: Int)
@@ -86,12 +68,8 @@ private val Activity.viewFinder: Activity.(Int) -> View?
     get() = { findViewById(it) }
 private val Dialog.viewFinder: Dialog.(Int) -> View?
     get() = { findViewById(it) }
-private val DialogFragment.viewFinder: DialogFragment.(Int) -> View?
-    get() = { dialog?.findViewById(it) ?: view?.findViewById(it) }
 private val SupportDialogFragment.viewFinder: SupportDialogFragment.(Int) -> View?
     get() = { dialog?.findViewById(it) ?: view?.findViewById(it) }
-private val Fragment.viewFinder: Fragment.(Int) -> View?
-    get() = { view.findViewById(it) }
 private val SupportFragment.viewFinder: SupportFragment.(Int) -> View?
     get() = { view!!.findViewById(it) }
 private val ViewHolder.viewFinder: ViewHolder.(Int) -> View?
