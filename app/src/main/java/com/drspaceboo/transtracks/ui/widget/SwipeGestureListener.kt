c97dae5685084c25193c638629c5f5db5159def2
Cassie Wilson <contact@transtrack.app>
2023-10-14 21:13:15 +1100

    Fix build issue

--- app/src/main/java/com/drspaceboo/transtracks/ui/widget/SwipeGestureListener.kt
+++ app/src/main/java/com/drspaceboo/transtracks/ui/widget/SwipeGestureListener.kt
@@ -15,12 +15,8 @@ import android.view.MotionEvent
 
 open class SwipeGestureListener : GestureDetector.SimpleOnGestureListener() {
     override fun onFling(
-        e1: MotionEvent?, e2: MotionEvent, velocityX: Float, velocityY: Float
+        e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float
     ): Boolean {
-        if (e1 == null) {
-            return false
-        }
-
         val dx = Math.abs(e1.x - e2.x)
         val dy = Math.abs(e1.y - e2.y)
         val absVelocityX = Math.abs(velocityX)
